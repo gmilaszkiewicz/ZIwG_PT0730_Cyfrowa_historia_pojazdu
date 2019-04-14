4b23aabf67c6516364e6b837819edfd81d4d8b26
maciej.kucia <maciej.kucia@capgemini.com>
2019-04-14 17:37:27 +0200

    styling appbar

--- src/components/bars/DrawerBar.js
+++ src/components/bars/DrawerBar.js
@@ -1,72 +1,109 @@
-import React, { Component } from 'react';
-import {ListItemText, ListItem, Divider, List, Drawer, Toolbar, Link} from '@material-ui/core'
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import {MyCars} from './MyCars'
-import styled from 'styled-components'
-import {StyledCarList as CarList} from './../car-card/CarList'
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {
+  ListItemText,
+  ListItem,
+  Divider,
+  List,
+  Drawer,
+  Toolbar
+} from "@material-ui/core";
+import styled from "styled-components";
 
-const drawerWidth = 240;
-
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import { MyCars } from "./MyCars";
+import { StyledCarList as CarList } from "./../car-card/CarList";
+import zIndex from "@material-ui/core/styles/zIndex";
+import TopBar from "./TopBar";
 const StyledDrawer = styled(Drawer)`
-    position: absolute;
-    width: "drawerWidth";
-    flex-shrink: 0;
-    z-index: -1;
-`
-
-const routes = [
-    {
-      path: "/cards",
-      exact: true,
-      sidebar: () => <CarList />,
-      main: () => <h2>Home</h2>
-    },
-    {
-      path: "/bubblegum",
-      sidebar: () => <div>bubblegum!</div>,
-      main: () => <h2>Bubblegum</h2>
-    },
-    {
-      path: "/shoelaces",
-      sidebar: () => <div>shoelaces!</div>,
-      main: () => <h2>Shoelaces</h2>
-    }
-  ];
-
-class DrawerBar extends Component {
-  
-    render(){
-        return (
-            <Router>
-                <StyledDrawer variant="permanent">
-                <Toolbar />
-                <List>
-                    {['Show my cars', 'Car services'].map((text, index) => (
-                    // <Link to={MyCars}>
-                        <ListItem button key={text}>
-                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItem>
-                    // {/* </Link> */}
-                    ))}
-                </List>
-                <Divider />
-                <List>
-                    {['Info'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-
-                    </ListItem>
-                    ))}
-                </List>
-                </StyledDrawer>
-            </Router>
-        );
-    }
+  .drawer {
+    top: 64px;
   }
+  position: absolute;
+  width: "drawerWidth";
+  flex-shrink: 0;
+  z-index: 1300;
+`;
 
-  export default DrawerBar;
+const styles = {
+  navBar: { top: TopBar.height }
+};
+
+// Each logical "route" has two components, one for
+// the sidebar and one for the main area. We want to
+// render both of them in different places when the
+// path matches the current URL.
+const routes = [
+  {
+    path: "/cars",
+    exact: true,
+    sidebar: () => <MyCars />,
+    main: () => <MyCars />
+  },
+  {
+    path: "/bubblegum",
+    sidebar: () => <div>bubblegum!</div>,
+    main: () => <h2>Bubblegum</h2>
+  },
+  {
+    path: "/shoelaces",
+    sidebar: () => <div>shoelaces!</div>,
+    main: () => <h2>Shoelaces</h2>
+  }
+];
+
+function SidebarExample() {
+  return (
+    <Router>
+      <div style={{ display: "flex" }}>
+        <div
+          style={{
+            padding: "10px",
+            width: "40%",
+            background: "#f0f0f0"
+          }}
+        >
+          <StyledDrawer variant="permanent" className="drawer">
+            <Toolbar />
+            <List>
+              {["Show my cars", "Car services"].map((text, index) => (
+                <Link to={`${routes[index].path}`}>
+                  <ListItem button key={text}>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                </Link>
+              ))}
+            </List>
+          </StyledDrawer>
+
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.sidebar}
+            />
+          ))}
+        </div>
+
+        <div style={{ flex: 1, padding: "10px" }}>
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.main}
+            />
+          ))}
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default SidebarExample;
--- src/components/bars/TopBar.js
+++ src/components/bars/TopBar.js
@@ -1,86 +1,95 @@
-import React, {Component} from 'react'
-import {AppBar, Toolbar, Typography, Menu, MenuItem, Grow } from '@material-ui/core'
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import HomeIcon from '@material-ui/icons/Home'
-import IconButton from '@material-ui/core/IconButton';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Menu,
+  MenuItem,
+  Grow
+} from "@material-ui/core";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import HomeIcon from "@material-ui/icons/Home";
+import IconButton from "@material-ui/core/IconButton";
+import styled from "styled-components";
 
 const StyledAppBar = styled(AppBar)`
-    position:absolute;
-    flex-grow: 1;
-`
+  position: absolute;
+  flex-grow: 1;
+
+`;
 
 const StyledMenuButton = styled(IconButton)`
-    margin-left: -12px;
-    margin-right: 20px;
-`
+  margin-left: -12px;
+  margin-right: 20px;
+`;
 
 const StyledTitle = styled(Typography)`
-    display: 'sm';
-`
+  display: "sm";
+`;
 const StyledDiv = styled.div`
-    flex-grow: 1;
-`
+  flex-grow: 1;
+`;
 
-class TopBar extends Component{
 
-    constructor(){
-        super()
-        this.state = {
-            anchorEl: null,
-        };
-    }
-    
-    handleProfileMenuOpen = event => {
-       this.setState({ anchorEl: event.currentTarget });
+
+class TopBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      anchorEl: null
     };
-    
-    handleMenuClose = () => {
-       this.setState({ anchorEl: null });
-    };
+  }
 
-    render(){
+  handleProfileMenuOpen = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
-        const isMenuOpen = Boolean(this.state.anchorEl);
-        const { classes } = this.props;
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+  };
 
-        const renderMenu = (
-            <Menu
-              anchorEl={this.state.anchorEl}
-              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-              open={isMenuOpen}
-              onClose={this.handleMenuClose}
+  render() {
+    const isMenuOpen = Boolean(this.state.anchorEl);
+    const { classes } = this.props;
+
+    const renderMenu = (
+      <Menu
+        anchorEl={this.state.anchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isMenuOpen}
+        onClose={this.handleMenuClose}
+      >
+        <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+        <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
+      </Menu>
+    );
+
+    return (
+      <div>
+        <StyledAppBar variant="dark">
+          <Toolbar>
+            <StyledMenuButton color="inherit" aria-label="Home">
+              <HomeIcon />
+            </StyledMenuButton>
+            <StyledTitle variant="h6" color="inherit" noWrap>
+              Our title
+            </StyledTitle>
+            <StyledDiv />
+            <IconButton
+              aria-owns={isMenuOpen ? "material-appbar" : undefined}
+              aria-haspopup="true"
+              onClick={this.handleProfileMenuOpen}
+              color="inherit"
             >
-              <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
-              <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
-            </Menu>
-          );
-
-        return(
-        <div>
-            <StyledAppBar  variant="dark">
-            <Toolbar>
-                <StyledMenuButton color="inherit" aria-label="Home">
-                    <HomeIcon />
-                </StyledMenuButton>
-                <StyledTitle variant="h6" color="inherit" noWrap>
-                    Our title
-                </StyledTitle>
-                <StyledDiv />
-                    <IconButton
-                        aria-owns={isMenuOpen ? 'material-appbar' : undefined}
-                        aria-haspopup="true"
-                        onClick={this.handleProfileMenuOpen}
-                        color="inherit"
-                    >
-                        <AccountCircle />
-                    </IconButton>
-            </Toolbar>
-            </StyledAppBar>
-            {renderMenu}
-        </div>
-      )}
+              <AccountCircle />
+            </IconButton>
+          </Toolbar>
+        </StyledAppBar>
+        {renderMenu}
+      </div>
+    );
+  }
 }
 
 export default TopBar;
