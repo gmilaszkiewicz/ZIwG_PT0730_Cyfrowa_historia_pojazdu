e55e01bf362c2234e1bfac07ccfbb00925945863
matozog <226125@student.pwr.edu.pl>
2019-04-14 16:11:11 +0200

    merge

--- src/components/bars/DrawerBar.js
+++ src/components/bars/DrawerBar.js
@@ -5,6 +5,8 @@ import MailIcon from '@material-ui/icons/Mail';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import {MyCars} from './MyCars'
 import styled from 'styled-components'
+import {StyledCarList as CarList} from './../car-card/CarList'
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 const drawerWidth = 240;
 
@@ -15,34 +17,54 @@ const StyledDrawer = styled(Drawer)`
     z-index: -1;
 `
 
+const routes = [
+    {
+      path: "/cards",
+      exact: true,
+      sidebar: () => <CarList />,
+      main: () => <h2>Home</h2>
+    },
+    {
+      path: "/bubblegum",
+      sidebar: () => <div>bubblegum!</div>,
+      main: () => <h2>Bubblegum</h2>
+    },
+    {
+      path: "/shoelaces",
+      sidebar: () => <div>shoelaces!</div>,
+      main: () => <h2>Shoelaces</h2>
+    }
+  ];
+
 class DrawerBar extends Component {
   
     render(){
         return (
-            <StyledDrawer variant="permanent">
-
-            <Toolbar />
-            <List>
-                {['Show my cars', 'Car services'].map((text, index) => (
-                // <Link to={MyCars}>
+            <Router>
+                <StyledDrawer variant="permanent">
+                <Toolbar />
+                <List>
+                    {['Show my cars', 'Car services'].map((text, index) => (
+                    // <Link to={MyCars}>
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    // {/* </Link> */}
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {['Info'].map((text, index) => (
                     <ListItem button key={text}>
                         <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
                         <ListItemText primary={text} />
-                    </ListItem>
-                // {/* </Link> */}
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['Info'].map((text, index) => (
-                <ListItem button key={text}>
-                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                    <ListItemText primary={text} />
 
-                </ListItem>
-                ))}
-            </List>
-            </StyledDrawer>
+                    </ListItem>
+                    ))}
+                </List>
+                </StyledDrawer>
+            </Router>
         );
     }
   }
