7f517d6b0ed35030e2bd7f506a5ffafd2d40ea34
matozog <226125@student.pwr.edu.pl>
2019-06-02 11:05:46 +0200

    repair small bug

--- src/components/car/NewCarForm.js
+++ src/components/car/NewCarForm.js
@@ -80,7 +80,7 @@ export class NewCarForm extends Component {
     });
     await this.sleep(400); //no niestety :(
     this.setState({ imagesInBase64: imagesInBase64.join() });
-    setFieldValue("photos", imagesInBase64.join());
+    setFieldValue("photos", imagesInBase64.join(" "));
   };
 
   getCarDataFromAPI = (carData) => {
@@ -112,7 +112,7 @@ export class NewCarForm extends Component {
                 const data = await this.getCarDataFromAPI(values)
                 if(data[0]!==undefined){
                   this.props.handleOnClose()
-                  // this.saveCar(values,data[0]);
+                  this.saveCar(values,data[0]);
                   this.props.snackbar.showMessage(
                     "Successful added new car!", "success")
                 }else{
